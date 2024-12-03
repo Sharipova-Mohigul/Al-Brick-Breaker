ae08dbb44b35788ee320c54dbcf186e913382285
Mohigul <mohigulsharipova44@gmail.com>
2024-12-03 17:39:57 +0500

    main

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Brick Breaker game in Python using pygame. It will be only one player.
+Will use reinforcement learning to learn how to play.
+
+The game is vertical, so the paddle will move left or right.
+
+"""
