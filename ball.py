b6c867b04f75e407187632a5a65f36d1b141975f
Sevinch <sevincholimova902@gmail.com>
2024-12-06 11:49:34 +0500

    okay

--- ball.py
+++ ball.py
@@ -12,3 +12,15 @@ class Ball(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         # the ball spawn aleatory in the width of the screen
         self.rect.center = (random() * WIDTH, HEIGHT / 2)
+         self.speed_x = 5
+        self.speed_y = 5
+
+    def update(self, game_manager):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        # if it touches any of the other sides, it revolts.
+        if self.rect.top < 0:
+            self.speed_y *= -1
+        if self.rect.left < 0 or self.rect.right > WIDTH:
+            self.speed_x *= -1
